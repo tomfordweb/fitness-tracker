15200ee81a1f980f126aefef6e912f4e35b00ff4
Tom Ford <tomfordweb@gmail.com>
2022-04-13 20:21:42 -0400

    return 1rm ranges for table and graph

--- pages/api/calculator/one-rep-max.ts
+++ pages/api/calculator/one-rep-max.ts
@@ -12,69 +12,83 @@ export interface ApiSimpleCalculatedData {
   value: string;
   formula: string;
 }
+
+const calculateOneRms = (weight: number, reps: number) => ({
+  brzycki: {
+    title: "Brzycki",
+    value: (weight * (36 / (37 - reps))).toFixed(2),
+    formula: `${weight} * (36 / ( 37 ${reps}))`,
+  },
+  oconner: {
+    title: "O'Conner",
+    value: (weight * (1 + 0.025 * reps)).toFixed(2),
+    formula: `${weight} * (1. 0.025 + ${reps})`,
+  },
+  mayhew: {
+    title: "Mayhew",
+    value: (
+      (100 * weight) /
+      (52.2 + 41.9 * Math.pow(Math.E, -0.055 * reps))
+    ).toFixed(2),
+    formula: `100 * ${weight} / (52.2 + (41.9 * ${Math.E} ^-0.055 * ${reps}))`,
+  },
+  lombardi: {
+    title: "Lombardi",
+    value: (weight * Math.pow(reps, 0.1)).toFixed(2),
+    formula: `${weight} * ${reps} ^ 0.1`,
+  },
+  lander: {
+    title: "Lander",
+    value: ((100 * weight) / (101.4 - 2.67123 * reps)).toFixed(2),
+    formula: `(100 * ${weight}) / (101.4 - 2.67123 * ${reps})`,
+  },
+  wathan: {
+    title: "Wathan",
+    value: (
+      (100 * weight) /
+      (46.8 + 53.8 * Math.pow(Math.E, -0.075 * reps))
+    ).toFixed(2),
+    formula: `(100 * ${weight}) / (46.8 + (53.8 * ${Math.exp(-0.075).toFixed(
+      2
+    )}* ${reps}))`,
+  },
+  epley: {
+    title: "Epley",
+    value: (weight * (1 + 0.0333 * reps)).toFixed(2),
+    formula: `${weight} * (1 + 0.0333 * ${reps})`,
+  },
+});
+interface Result {
+  brzycki: ApiSimpleCalculatedData;
+  oconner: ApiSimpleCalculatedData;
+  mayhew: ApiSimpleCalculatedData;
+  lombardi: ApiSimpleCalculatedData;
+  lander: ApiSimpleCalculatedData;
+  wathan: ApiSimpleCalculatedData;
+  epley: ApiSimpleCalculatedData;
+}
 export default validate(
   { query: schema },
   (
     req: NextApiRequest,
     res: NextApiResponse<{
-      brzycki: ApiSimpleCalculatedData;
-      oconner: ApiSimpleCalculatedData;
-      mayhew: ApiSimpleCalculatedData;
-      lombardi: ApiSimpleCalculatedData;
-      lander: ApiSimpleCalculatedData;
-      wathan: ApiSimpleCalculatedData;
-      epley: ApiSimpleCalculatedData;
+      result: Result;
+      graph: Result[];
     }>
   ) => {
     const weight = parseInt(req.query.weight as string);
     const reps = parseInt(req.query.reps as string);
 
+    const graph = Array.from({ length: 10 }, (_, i) => i + 1).map(
+      (i: number) => ({
+        ...calculateOneRms(weight, i + 1),
+        reps: i,
+      })
+    );
+
     res.status(200).json({
-      brzycki: {
-        title: "Brzycki",
-        value: (weight * (36 / (37 - reps))).toFixed(2),
-        formula: `${weight} * (36 / ( 37 ${reps}))`,
-      },
-      oconner: {
-        title: "O'Conner",
-        value: (weight * (1 + 0.025 * reps)).toFixed(2),
-        formula: `${weight} * (1. 0.025 + ${reps})`,
-      },
-      mayhew: {
-        title: "Mayhew",
-        value: (
-          (100 * weight) /
-          (52.2 + 41.9 * (Math.E - 0.055) * reps)
-        ).toFixed(2),
-        formula: `100 * ${weight} / (52.2 + (41.9 * ${(Math.E - 0.055).toFixed(
-          2
-        )} * ${reps}))`,
-      },
-      lombardi: {
-        title: "Lombardi",
-        value: Math.pow(weight * reps, 0.1).toFixed(2),
-        formula: `${weight} * ${reps} ^ 0.1`,
-      },
-      lander: {
-        title: "Lander",
-        value: ((100 * weight) / (101.4 - 2.67123 * reps)).toFixed(2),
-        formula: `(100 * ${weight}) / (101.4 - 2.67123 * ${reps})`,
-      },
-      wathan: {
-        title: "Wathan",
-        value: (
-          (100 * weight) /
-          (46.8 + 53.8 * Math.exp(-0.075) * reps)
-        ).toFixed(2),
-        formula: `(100 * ${weight}) / (46.8 + (53.8 * ${Math.exp(
-          -0.075
-        ).toFixed(2)}* ${reps}))`,
-      },
-      epley: {
-        title: "Epley",
-        value: (weight * (1 + 0.0333 * reps)).toFixed(2),
-        formula: `${weight} * (1 + 0.0333 * ${reps})`,
-      },
+      result: calculateOneRms(weight, reps),
+      graph,
     });
   }
 );
