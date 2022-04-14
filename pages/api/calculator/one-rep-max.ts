1c48915a5554a39c94b2fd161f173fec8f51bd83
Tom Ford <tomfordweb@gmail.com>
2022-04-13 21:09:13 -0400

    add table

--- pages/api/calculator/one-rep-max.ts
+++ pages/api/calculator/one-rep-max.ts
@@ -11,20 +11,33 @@ export interface ApiSimpleCalculatedData {
   title: string;
   value: string;
   formula: string;
+  percentages: { percent: number; weight: number }[];
 }
 
-const calculateOneRms = (weight: number, reps: number) => ({
-  brzycki: {
+const calculatePercentageRange = (value: number) => {
+  /**
+   * Subtract 5 from 100..10 times
+   */
+  return Array.from({ length: 10 }, (_, i) => i + 1)
+    .map((i) => 100 - i * 5)
+    .map((percent) => ({
+      percent,
+      weight: value * (percent / 100),
+    }));
+};
+
+const calculateOneRms = (weight: number, reps: number) => [
+  {
     title: "Brzycki",
     value: (weight * (36 / (37 - reps))).toFixed(2),
     formula: `${weight} * (36 / ( 37 ${reps}))`,
   },
-  oconner: {
+  {
     title: "O'Conner",
     value: (weight * (1 + 0.025 * reps)).toFixed(2),
     formula: `${weight} * (1. 0.025 + ${reps})`,
   },
-  mayhew: {
+  {
     title: "Mayhew",
     value: (
       (100 * weight) /
@@ -32,17 +45,17 @@ const calculateOneRms = (weight: number, reps: number) => ({
     ).toFixed(2),
     formula: `100 * ${weight} / (52.2 + (41.9 * ${Math.E} ^-0.055 * ${reps}))`,
   },
-  lombardi: {
+  {
     title: "Lombardi",
     value: (weight * Math.pow(reps, 0.1)).toFixed(2),
     formula: `${weight} * ${reps} ^ 0.1`,
   },
-  lander: {
+  {
     title: "Lander",
     value: ((100 * weight) / (101.4 - 2.67123 * reps)).toFixed(2),
     formula: `(100 * ${weight}) / (101.4 - 2.67123 * ${reps})`,
   },
-  wathan: {
+  {
     title: "Wathan",
     value: (
       (100 * weight) /
@@ -52,43 +65,30 @@ const calculateOneRms = (weight: number, reps: number) => ({
       2
     )}* ${reps}))`,
   },
-  epley: {
+  {
     title: "Epley",
     value: (weight * (1 + 0.0333 * reps)).toFixed(2),
     formula: `${weight} * (1 + 0.0333 * ${reps})`,
   },
-});
-interface Result {
-  brzycki: ApiSimpleCalculatedData;
-  oconner: ApiSimpleCalculatedData;
-  mayhew: ApiSimpleCalculatedData;
-  lombardi: ApiSimpleCalculatedData;
-  lander: ApiSimpleCalculatedData;
-  wathan: ApiSimpleCalculatedData;
-  epley: ApiSimpleCalculatedData;
-}
+];
 export default validate(
   { query: schema },
-  (
-    req: NextApiRequest,
-    res: NextApiResponse<{
-      result: Result;
-      graph: Result[];
-    }>
-  ) => {
+  (req: NextApiRequest, res: NextApiResponse<ApiSimpleCalculatedData[]>) => {
     const weight = parseInt(req.query.weight as string);
     const reps = parseInt(req.query.reps as string);
 
-    const graph = Array.from({ length: 10 }, (_, i) => i + 1).map(
-      (i: number) => ({
-        ...calculateOneRms(weight, i + 1),
-        reps: i,
-      })
-    );
+    let result = calculateOneRms(weight, reps);
 
-    res.status(200).json({
-      result: calculateOneRms(weight, reps),
-      graph,
-    });
+    let returnResult: ApiSimpleCalculatedData[] = result.map((result) => ({
+      ...result,
+      percentages: calculatePercentageRange(parseFloat(result.value || "")),
+    }));
+
+    // .map((i: number) => ({
+    //   ...calculateOneRms(weight, i + 1),
+    //   reps: i,
+    // }));
+
+    res.status(200).json(returnResult);
   }
 );
--- pages/strength-calculators/one-rep-max-calculator.tsx
+++ pages/strength-calculators/one-rep-max-calculator.tsx
@@ -1,26 +1,28 @@
-import { Container, Grid, Typography } from "@mui/material";
+import {
+  Container,
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import { NextPage } from "next";
 import { useState } from "react";
 import { OneRmCalculator } from "../../component/calculator/one-rm-calculator";
 import { PageTitle } from "../../component/page-title";
 import { ResultCard } from "../../component/result-card";
-import { SimpleCalculatedData } from "../../types";
+import { ApiSimpleCalculatedData } from "../api/calculator/one-rep-max";
 
-export interface OneRmCalculations {
-  brzycki: SimpleCalculatedData & { title: string };
-  oconner: SimpleCalculatedData & { title: string };
-  mayhew: SimpleCalculatedData & { title: string };
-  lombardi: SimpleCalculatedData & { title: string };
-  lander: SimpleCalculatedData & { title: string };
-  wathan: SimpleCalculatedData & { title: string };
-  epley: SimpleCalculatedData & { title: string };
-}
 export const OneRepMaxCalculatorPage: NextPage = () => {
-  const [oneRms, setOneRms] = useState<OneRmCalculations | null>(null);
-  const [activeCard, setActiveCard] = useState<
-    (SimpleCalculatedData & { title: string }) | null
-  >(null);
+  const [oneRms, setOneRms] = useState<ApiSimpleCalculatedData[]>([]);
+  const [activeCard, setActiveCard] = useState<ApiSimpleCalculatedData | null>(
+    null
+  );
 
   return (
     <>
@@ -29,16 +31,21 @@ export const OneRepMaxCalculatorPage: NextPage = () => {
         <Box sx={{ mb: 5 }}>
           <OneRmCalculator onSuccess={setOneRms} />
         </Box>
-        {oneRms && (
+        {oneRms && oneRms.length > 0 && (
           <>
             <Typography>
               Below are the one rep max calculations for the provided weight and
               reps. You can click a card below to see the percentage against the
               rep range table.
             </Typography>
-            <Grid sx={{ mt: 3 }} container spacing={2}>
-              {Object.values(oneRms).map((oneRm, i) => (
-                <Grid key={i} item xs={3} onClick={() => setActiveCard(oneRm)}>
+            <Grid sx={{ my: 3 }} container spacing={2}>
+              {oneRms.map((oneRm) => (
+                <Grid
+                  key={oneRm.title}
+                  item
+                  xs={3}
+                  onClick={() => setActiveCard(oneRm)}
+                >
                   <ResultCard
                     active={activeCard?.title === oneRm.title}
                     {...oneRm}
@@ -46,139 +53,47 @@ export const OneRepMaxCalculatorPage: NextPage = () => {
                 </Grid>
               ))}
             </Grid>
+            <TableContainer component={Paper}>
+              <Table
+                sx={{ minWidth: 650 }}
+                size="small"
+                aria-label="a dense table"
+              >
+                <TableHead>
+                  <TableRow sx={{ bgcolor: "secondary.main" }}>
+                    <TableCell>Formula</TableCell>
+                    {oneRms[0].percentages.map((header) => (
+                      <TableCell align="right" sx={{ fontWeight: "bold" }}>
+                        {header.percent}%
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {oneRms.map((row) => (
+                    <TableRow
+                      key={row.title}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell
+                        component="th"
+                        scope="row"
+                        sx={{ bgcolor: "secondary.light" }}
+                      >
+                        {row.title}
+                      </TableCell>
+
+                      {oneRms[0].percentages.map((header) => (
+                        <TableCell align="right">
+                          {header.weight.toFixed(1)}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </>
-        )}
-        {activeCard && (
-          <table>
-            <thead>
-              <tr>
-                <th>Repetitions</th>
-                <th>Percentage of 1RM</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>100%</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>97%</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>94%</td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>92%</td>
-              </tr>
-              <tr>
-                <td>5</td>
-                <td>89%</td>
-              </tr>
-              <tr>
-                <td>6</td>
-                <td>86%</td>
-              </tr>
-              <tr>
-                <td>7</td>
-                <td>83%</td>
-              </tr>
-              <tr>
-                <td>8</td>
-                <td>81%</td>
-              </tr>
-              <tr>
-                <td>9</td>
-                <td>78%</td>
-              </tr>
-              <tr>
-                <td>10</td>
-                <td>75%</td>
-              </tr>
-              <tr>
-                <td>11</td>
-                <td>73%</td>
-              </tr>
-              <tr>
-                <td>12</td>
-                <td>71%</td>
-              </tr>
-              <tr>
-                <td>13</td>
-                <td>70%</td>
-              </tr>
-              <tr>
-                <td>14</td>
-                <td>68%</td>
-              </tr>
-              <tr>
-                <td>15</td>
-                <td>67%</td>
-              </tr>
-              <tr>
-                <td>16</td>
-                <td>65%</td>
-              </tr>
-              <tr>
-                <td>17</td>
-                <td>64%</td>
-              </tr>
-              <tr>
-                <td>18</td>
-                <td>63%</td>
-              </tr>
-              <tr>
-                <td>19</td>
-                <td>61%</td>
-              </tr>
-              <tr>
-                <td>20</td>
-                <td>60%</td>
-              </tr>
-              <tr>
-                <td>21</td>
-                <td>59%</td>
-              </tr>
-              <tr>
-                <td>22</td>
-                <td>58%</td>
-              </tr>
-              <tr>
-                <td>23</td>
-                <td>57%</td>
-              </tr>
-              <tr>
-                <td>24</td>
-                <td>56%</td>
-              </tr>
-              <tr>
-                <td>25</td>
-                <td>55%</td>
-              </tr>
-              <tr>
-                <td>26</td>
-                <td>54%</td>
-              </tr>
-              <tr>
-                <td>27</td>
-                <td>53%</td>
-              </tr>
-              <tr>
-                <td>28</td>
-                <td>52%</td>
-              </tr>
-              <tr>
-                <td>29</td>
-                <td>51%</td>
-              </tr>
-              <tr>
-                <td>30</td>
-                <td>50%</td>
-              </tr>
-            </tbody>
-          </table>
         )}
       </Container>
     </>
