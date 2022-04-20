616822d2d5c4d5242f59baec93153c7d6f3e33da
Tom Ford <tomfordweb@gmail.com>
2022-04-19 22:06:23 -0400

    push tag events on page view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,24 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../component/layout";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const handleRouteChange = (url: string) => {
+    (window as any).gtag("config", process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <Layout>
       <Component {...pageProps} />
