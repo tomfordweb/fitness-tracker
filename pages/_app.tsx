c8cbef64f9fc2a5e87e7a6573d91a2e9a2093a7f
Tom Ford <tomfordweb@gmail.com>
2022-04-19 22:01:57 -0400

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../component/layout";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
     </Layout>
   );
 }
